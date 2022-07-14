c5256adac290dd0e8e1eddf340321335bf8ea1a8
myb12 <mohammadyasinbappy@outlook.com>
2022-07-14 20:26:41 +0600

    Header is completed

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
+import ButtonPrimary from '../buttonPrimary/ButtonPrimary';
+import './Header.css';
+import Logo from '../../assets/images/logo.png'
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg ">
             <div className="container">
                 <a className="navbar-brand" href="/#">
-                    <img src="https://softcent.eu/wp-content/uploads/2021/10/logo-v2.png" alt="" width="50" />
+                    <img src={Logo} alt="" width="100" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ flexGrow: 0 }}>
+                    <ul className="navbar-nav  mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
+                            <a className="nav-link" aria-current="page" href="/#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/#">Link</a>
+                            <a className="nav-link" href="/#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#">Work</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#">Premium products</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#">Blog</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <button className="btn btn-outline-success" type="submit">Start a project</button>
-                    </form>
+                    <ButtonPrimary title='Start a project' variant='header' />
                 </div>
             </div>
         </nav>
--- src/shared/buttonPrimary/ButtonPrimary.jsx
+++ src/shared/buttonPrimary/ButtonPrimary.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './ButtonPrimary.css'
+import './ButtonPrimary.css';
 
-const ButtonPrimary = ({ title }) => {
+// '#3088FF' : '#3088FF' : '#2D5BFF'
+
+const ButtonPrimary = ({ title, variant }) => {
     return (
-        <button className='btn button-primary'>
+        <button
+            className='btn button-primary'
+            style={{ backgroundColor: variant === 'light' ? '#3088ff29' : variant === 'header' ? '#3088FF' : '#2D5BFF', color: variant === 'light' ? '#000' : '#fff' }}>
             {title}
         </button>
     );
